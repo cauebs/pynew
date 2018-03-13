f770d7f04f6b0180241d8d91bed2b4043985acee
Cauê Baasch de Souza <cauebs@protonmail.com>
2018-03-13 16:17:51 -0300

    Prepared setup.py for pypi upload

--- setup.py
+++ setup.py
@@ -3,7 +3,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='pynew',
+    description='Automate Python project creation',
     version='0.1.1',
+    author='Cauê Baasch de Souza',
+    author_email='cauebs@pm.me',
+    url='https://github.com/cauebs/pynew',
+    license='MIT',
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
     packages=find_packages(),
     install_requires=['click'],
     entry_points={
